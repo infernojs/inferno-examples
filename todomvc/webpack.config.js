937ac1732d51a4631ba0b8d421c6e74728a1bdfd
Luke Edwards <luke.edwards05@gmail.com>
2016-12-07 20:29:06 -0800

    inject webpack NODE_ENV; always prod

--- todomvc/webpack.config.js
+++ todomvc/webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -16,6 +18,13 @@ module.exports = {
 			}
 		}]
 	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+	],
 	devServer: {
 		port: process.env.PORT || 8000
 	}
