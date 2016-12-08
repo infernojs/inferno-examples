f80e66e5c03cbe85735785a5290a873a25229de4
Luke Edwards <luke.edwards05@gmail.com>
2016-12-08 12:53:26 -0800

    fix nested routes; User inherits Users

--- routing/src/index.js
+++ routing/src/index.js
@@ -6,14 +6,15 @@ import {App, Home, User, Users, NoMatch} from './tags';
 const history = createHistory();
 
 const routes = (
-  <Router history={ history }>
-    <Route component={ App }>
-      <Route path="/" component={ Home } />
-      <Route path="/users" component={ Users } />
-      <Route path="/users/:username" component={ User } />
-      <Route path="*" component={ NoMatch } />
-    </Route>
-  </Router>
+	<Router history={ history }>
+		<Route component={ App }>
+			<Route path="/" component={ Home } />
+			<Route path="/users" component={ Users }>
+				<Route path="/:username" component={ User } />
+			</Route>
+			<Route path="*" component={ NoMatch } />
+		</Route>
+	</Router>
 );
 
 render(routes, document.getElementById('app'));
